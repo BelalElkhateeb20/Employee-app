772d3892198f2d3786b686f5a4865444175ab519
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-02 18:16:08 +0200

    Add project files.

---
+++ ConsoleApp3/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    //<class modifiers> →(public,internal(default),
+    public class Employee
+    {
+        public static double TAX = .03;
+        //<Access modifier> public ,private,protected
+        public  string Fname;
+        public  string Lname;
+        public  double Wage;
+        public  double LoggedHours;
+        public Employee()
+        {}
+        private double caluc_salary() => Wage * LoggedHours;                     //if the method contains one line we dont need→{}
+        private double caluc_TAXamount() =>   caluc_salary() * Employee.TAX;       //if the method contains one line we dont need→{}
+        private double caluc_netsalary() => caluc_salary() - caluc_TAXamount();    //if the method contains one line we dont need→{}
+        public  string display()
+        {
+            return
+                   $"\nMy name is : {Fname}" +
+                   $"\nMy name is : {Lname}" +
+                   $"\nwage :{Wage}" +
+                   $"\nloogedhour :{LoggedHours}" +
+                   $"\nsalary is : {caluc_salary()}" +
+                   $"\nDeductable TAX ({Employee.TAX * 100}%)  Amount : {caluc_salary()}" +
+                   $"\nNetsalary is : {caluc_netsalary()}" +
+                   "\n-----------------------\n";
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,79 @@
+﻿using ConsoleApp3;
+using System;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee[] emps = new Employee[2];
+            Console.Write("TAX: ");
+            Employee.TAX = Convert.ToDouble(Console.ReadLine());
+            Employee ob1 = new ();
+            Console.Write("enter you fname : ");
+            ob1.Fname = Console.ReadLine();
+            Console.Write("enter you Lname : ");
+            ob1.Lname = Console.ReadLine();
+            Console.Write($"wage :");
+            ob1.Wage = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"loogedhour :");
+            ob1.LoggedHours = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*****************");
+            emps[0] = ob1;
+
+            Console.Write("TAX: ");
+            Employee.TAX = Convert.ToDouble(Console.ReadLine());
+
+            Employee ob2 = new();
+            Console.Write("enter you fname : ");
+            ob2.Fname = Console.ReadLine();
+            Console.Write("enter you Lname : ");
+            ob2.Lname = Console.ReadLine();
+            Console.Write($"wage :");
+            ob2.Wage = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"loogedhour :");
+            ob2.LoggedHours = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("******************");
+            emps[1] = ob2;
+
+            ob2.display();
+
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(emp.display());
+            }
+            Console.ReadKey();
+            //Program ob1 = new Program();
+
+            //var a = 10;
+            //when we want it witout initalization we put (out)befor int age and  
+            //ob1.num_calc(a);//by value
+            //ob1.num_calco(ref a);//by reference 
+            //Console.WriteLine(a);//by value
+            //Console.WriteLine( a);//by reference 
+            //    var number = "123345d";
+            //    int num;
+            //    if (int.TryParse(number, out num)) { Console.WriteLine($"valid num {num}"); }
+            //    else
+            //    {
+
+            //        Console.WriteLine("not valid number");
+            //    }
+            //    Console.ReadKey();
+
+            //}
+            //public void num_calc (int age)
+            //    {
+            //    age = age + 10;
+            //    }
+            //public void num_calco (ref int age)
+            //    {
+            //    age= age + 10;
+
+            //    }
+        }
+    }
+}
